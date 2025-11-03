ec7af4c592748194f212aecfd2d62d402c78cd7f
iop0175 <iop0175@naver.com>
2025-11-03 13:00:53 +0900

    S

--- day1/src/kr/or/ddit/basic/VariableInitializationExample.java
+++ day1/src/kr/or/ddit/basic/VariableInitializationExample.java
@@ -8,7 +8,7 @@ public class VariableInitializationExample {
 		value = 20 ;
 		// 변수갑을 읽고 +10 하는 산수연산을 수행
 		// 연산의 결과값을 변수 result의 초기값으로 저장
-		int resuly = value + 10;
+		int resuly = value + 102;
 		
 		System.out.println(resuly);
 	}
