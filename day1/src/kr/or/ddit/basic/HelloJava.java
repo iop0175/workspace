5900b079938bd6f43fb5a7122ee7fc5eccd8a7cf
iop0175 <iop0175@naver.com>
2025-11-03 12:45:12 +0900

    1

---
+++ day1/src/kr/or/ddit/basic/HelloJava.java
@@ -0,0 +1,12 @@
+package kr.or.ddit.basic;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("HelloJava");
+		int s = 5;
+		double age = 0.2;
+		int x,y,z;
+		System.out.println(s+age);
+		
+	}
+}
